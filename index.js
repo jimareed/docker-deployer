ccde4f91eb55083790ff960d6cd70e1c47b1c2ab
jimareed <jim.a.reed@gmail.com>
2015-05-10 12:20:59 -0400

    stub version

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var DEFAULT_PORT = 8080;
+var PORT = process.env.PORT || DEFAULT_PORT;
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World\n');
+});
+app.listen(PORT)
+console.log('Running on http://localhost:' + PORT);
