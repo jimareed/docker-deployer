b83c187881ebd52d65ddedf7604f7d6de0cf6fa3
jimareed <jim.a.reed@gmail.com>
2015-05-12 19:13:42 -0400

    added shelljs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+require('shelljs/global');
+
 var express = require('express');
 var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
+
+if (exec('echo hello') !== 0) {
+	console.log('exec failed');
+} else {
+	console.log('exec success');
+}
+
 app.get('/', function (req, res) {
   res.send('Hello World\n');
 });
