9300651736c34fb1b8c2d46cbc796189a4488bbf
jimareed <jim.a.reed@gmail.com>
2015-05-12 22:01:08 -0400

    added run.sh

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-require('shelljs/global');
-
 var express = require('express');
+var shell = require('shelljs');
+
 var DEFAULT_PORT = 9080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
 
-if (exec('docker ps | grep helloworld >t.out') !== 0) {
-	console.log('exec failed');
-} else {
-	console.log('exec success');
+if (shell.exec('./run.sh').code != 0) {
+  console.log('error running script');
 }
 
 app.get('/', function (req, res) {
   res.send('Hello World\n');
 });
+
 app.post('/deploy',function(req,res){
   console.log("Deploy");
-  res.end("yes");
+  if (shell.exec('./run.sh').code != 0) {
+    console.log('error running script');
+  }
+  res.end("ok");
 });
+
 app.listen(PORT)
 console.log('Running on http://localhost:' + PORT);
