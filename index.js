13af9afc3ba0aab57f7330b24512ae7bdeaee39d
root <root@ip-172-31-37-211.us-west-2.compute.internal>
2015-05-18 20:02:34 +0000

    updated refresh script

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var shell = require('shelljs');
-var Log = require('log');
-var fs = require('fs');
-
-var stream = fs.createReadStream(__dirname + '/deployer.log');
-var log = Log('info', stream);
 
 var DEFAULT_PORT = 9080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
 
 app.post('/deploy',function(req,res){
-  log.info('deploy');
+  console.log('deploy');
   if (shell.exec('./deploy.sh').code != 0) {
-    log.error('error deploying image');
+    console.log('error deploying image');
   }
   res.end("ok");
 });
 
 app.listen(PORT)
-log.info('Running on http://localhost:' + PORT);
+console.log('Running on http://localhost:' + PORT);
