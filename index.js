985c36791856bf22593b7ce1881d351e1320d5a4
root <root@ip-172-31-37-211.us-west-2.compute.internal>
2015-05-18 19:34:05 +0000

    added logging

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var shell = require('shelljs');
+var Log = require('log');
+var fs = require('fs');
+
+var stream = fs.createReadStream(__dirname + '/deployer.log');
+var log = Log('info', stream);
 
 var DEFAULT_PORT = 9080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
 
 app.post('/deploy',function(req,res){
-  console.log("Deploy");
+  log.info('deploy');
   if (shell.exec('./deploy.sh').code != 0) {
-    console.log('error running script');
+    log.error('error deploying image');
   }
   res.end("ok");
 });
 
 app.listen(PORT)
-console.log('Running on http://localhost:' + PORT);
+log.info('Running on http://localhost:' + PORT);
