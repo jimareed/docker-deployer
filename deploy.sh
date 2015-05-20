006623d6eaf82c49831f9a53fa7cbd5babed3970
root <root@ip-172-31-37-211.us-west-2.compute.internal>
2015-05-20 11:09:52 +0000

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-docker stop helloworld
-docker rm helloworld
-docker pull jimareed/helloworld
-docker run --name helloworld -d -p 8080:8080 jimareed/helloworld
+docker stop $2
+docker rm $2
+echo deploying $1 $2
+docker pull $1/$2
+docker run --name $2 -d -p 8080:8080 $1/$2
