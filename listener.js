8498e74a05994a1cdb12ac4b7483a93466ea86cc
jimareed <jim.a.reed@gmail.com>
2015-05-21 12:07:07 +0000

    assume source port is 8080, log deploy time

--- listener.js
+++ listener.js
@@ -10,11 +10,13 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 app.use(bodyParser.json());
 
 app.post('/deploy/:port',function(req,res){
+  console.log('deploying ' + req.body.repository.owner + '/' + 
+              req.body.repository.name + ' at ' + Date().toString());
   if (shell.exec('./deploy.sh ' + 
                  req.body.repository.owner + ' ' + 
                  req.body.repository.name + ' ' +
                  req.params.port).code != 0) {
-    console.log('error deploying image');
+    console.log('error deploying ' + req.body.repository.name);
   }
   res.end("ok");
 });
